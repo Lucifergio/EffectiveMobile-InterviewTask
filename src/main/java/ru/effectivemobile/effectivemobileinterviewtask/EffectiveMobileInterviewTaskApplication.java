644421d64228a6523f4349448b85327707305731
aleksey <lexasalnikov@gmail.com>
2023-03-19 16:28:34 +0500

    Initial commit

---
+++ src/main/java/ru/effectivemobile/effectivemobileinterviewtask/EffectiveMobileInterviewTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.effectivemobile.effectivemobileinterviewtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveMobileInterviewTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveMobileInterviewTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/effectivemobile/effectivemobileinterviewtask/EffectiveMobileInterviewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.effectivemobile.effectivemobileinterviewtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileInterviewTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
